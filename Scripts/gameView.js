3e83a7b20da445f6ca58a209d1e5f777122f7488
mchach24 <hach24mc@gmail.com>
2015-11-07 16:15:33 -0500

    Added correct functionality for x and o's. Added html attribute height and width to each subGame.

--- Scripts/gameView.js
+++ Scripts/gameView.js
@@ -12,9 +12,11 @@ pageWindow.on('resize', updateBoardSize);
 
 function updateBoardSize() {
     subGameObject.height(subGameObject.width());
+
 }
 
-/*
+/**
+* public func
 * draws an x or o on a subGame or subGame square
 * @param {DOM Object} subGame JS (not jQuery) subGame clicked
 * @param {string} turn Current turn; 'X' or 'O'
@@ -27,14 +29,26 @@ function markBoard(subGame, turn, square) {
         drawO(subGame);
 }
 
+/**
+* private func
+* draws an x
+* @param {DOM Object} subGame JS (not jQuery) subGame clicked
+*/
 function drawX(subGame) {
     var ctx = subGame.getContext('2d');
     
-    ctx.fillText('X', 20, 20);
+    ctx.beginPath();
+    ctx.moveTo(5,5);
+    ctx.lineTo(subGame.width - 5, subGame.height - 5);
+    ctx.moveTo(subGame.width - 5, 5);
+    ctx.lineTo(5, subGame.height - 5);
+    ctx.stroke();
 }
 
 function drawO(subGame) {
     var ctx = subGame.getContext('2d');
     
-    ctx.fillText('O', 20, 20);
+    ctx.beginPath();
+    ctx.arc(subGame.width/2, subGame.height/2, subGame.width/2.25, 0, 2 * Math.PI);
+    ctx.stroke();
 }
