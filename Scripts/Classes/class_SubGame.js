e27e5d62e62e826b1cc4398aecc3abb2a11a5e08
Nathaniel Hachten <Nathan@Nathaniels-MacBook-Air.local>
2015-11-07 17:51:25 -0500

    Added functionality to subgame and game model. removed main game class

--- Scripts/Classes/class_SubGame.js
+++ Scripts/Classes/class_SubGame.js
@@ -1,4 +1,130 @@
-function SubGame()
+/**
+* class
+* SubGame class used by the gameModel
+* @param {string} id The unique id for the subgame in format r[0-2]c[0-2]
+*/
+function SubGame(id)
 {
+	this.id = id; // r[0-2]c[0-2] 
+	//this.canvas = canvas;
+	this.gameOver = false;
+	var idArray = [0,0,0,0,0,0,0,0,0]; //0 - empty, 1 - X, 2 - O
+	//this.active = true;
+	this.winner = 0; // 0 - game not over, 1 - X won, 2 - O won, 3 - tie
+    
+    /**
+	* @param {string} turn Whose turn is it? X or O
+	* @param {int} squareId The id for the square that the move was played 0-8
+	* @return {boolean} did the move go through or not
+	*/
+	this.movePlayed = function(turn, squareId)
+	{        
+		if (this.gameOver) return false;
+        
+        var moveIsValid = false;
+        
+        if (turn === 'X') {
+            playerId = 1;   
+        }
+        else {
+            playerId = 2;
+        }
+		
+		if (idArray[squareId] == 0) {
+            idArray[squareId] = playerId;
+            moveIsValid = true;
+		}
+		
+        this.detectGameOver;
+		return moveIsValid;
+		
+	}
+	
+	this.detectGameOver = function()
+	{
+		//alert(idArray);
+		//horizontal row 1 win check
+		if (idArray[0] != 0 && idArray[0] == idArray[1] && idArray[0] == idArray[2]) {
+			//alert("detect 1");
+			this.winner = idArray[0];
+		}
+		//horizontal row 2 win check
+		else if (idArray[3] != 0 && idArray[3] == idArray[4] && idArray[3] == idArray[5]) {
+			//alert("detect 2");
+			this.winner = idArray[3];
+		}
+		//horizontal row 3 win check
+		else if (idArray[6] != 0 && idArray[6] == idArray[7] && idArray[6] == idArray[8]) {
+			//alert("detect 3");
+			this.winner = idArray[6];
+		}
+		//vertical column 1 win check
+		else if (idArray[0] != 0 && idArray[0] == idArray[3] && idArray[0] == idArray[6]) {
+			//alert("detect 4");
+			this.winner = idArray[0];
+		}
+		//vertical column 2 win check
+		else if (idArray[1] != 0 && idArray[1] == idArray[4] && idArray[1] == idArray[7]) {
+			//alert("detect 5");
+			this.winner = idArray[1];
+		}
+		//vertical column 3 win check
+		else if (idArray[2] != 0 && idArray[2] == idArray[5] && idArray[2] == idArray[8]) {
+			//alert("detect 6");
+			this.winner = idArray[2];
+		}
+		//diagonal starting from top left win check
+		else if (idArray[0] != 0 && idArray[0] == idArray[4] && idArray[0] == idArray[8]) {
+			//alert("detect 7");
+			this.winner = idArray[0];
+		}
+		//diagonal starting from top right win check
+		else if (idArray[2] != 0 && idArray[2] == idArray[4] && idArray[2] == idArray[6]) {
+			//alert("detect 8");
+			this.winner = idArray[2];
+		}
+
+		var boardFull = true;
+		
+		for (i = 0; i < 9; i++)
+		{
+			if (idArray[i] == 0)
+			{
+				boardFull = false;
+				break;
+			}	
+		}
+
+        if (this.winner == 0)
+		{
+			if (boardFull) {
+				this.winner = 3;
+			}
+			else {
+				return;
+			}
+		}
+
+		this.gameOver = true;
+	}
+	
+	/*this.setActive = function ()
+	{
+		if (!this.gameOver)
+		{
+			this.active = true;
+			this.canvas.className = "field";
+		}
+	}*/
+	
+	/*this.setInactive = function ()
+	{
+		if (!this.gameOver)
+		{
+			this.active = false;
+			this.canvas.className = "inactive";
+		}
+		
+	}*/
 
 }
--- Scripts/gameModel.js
+++ Scripts/gameModel.js
@@ -2,18 +2,17 @@
 //var game = new MainGame();
 var subGames = [];
 
-/*
-subGames.push(new subGame(0, document.getElementById('f11')));
-subGames.push(new subGame(1, document.getElementById('f12')));
-subGames.push(new subGame(2, document.getElementById('f13')));
-subGames.push(new subGame(3, document.getElementById('f21')));
-subGames.push(new subGame(4, document.getElementById('f22')));
-subGames.push(new subGame(5, document.getElementById('f23')));
-subGames.push(new subGame(6, document.getElementById('f31')));
-subGames.push(new subGame(7, document.getElementById('f32')));
-subGames.push(new subGame(8, document.getElementById('f33')));
-*/
-var currentTurn = 'X'; 
+
+subGames.push(new SubGame(0));
+subGames.push(new SubGame(1));
+subGames.push(new SubGame(2));
+subGames.push(new SubGame(3));
+subGames.push(new SubGame(4));
+subGames.push(new SubGame(5));
+subGames.push(new SubGame(6));
+subGames.push(new SubGame(7));
+subGames.push(new SubGame(8));
+
 var gameOver = false;
 
 
@@ -21,22 +20,120 @@ var gameOver = false;
 * public
 * This function processes moves to determine if the move is valid and if it is
 * updates its internal game state
-* @param {string}   canvasID    The ID of the subgame/canvas where the move was played
+* @param {int}      subgameID   The ID of the subgame/canvas where the move was played
 * @param {string}   turn        A string for whose move it is
 * @param {int}      squareNum   An int for the square within the subgame where the move was played
 * @return {boolean}             Returns whether the move was valid or not
 */
-function movePlayed(canvasID, turn, squareNum)
+function movePlayed(subgameID, turn, squareNum)
 {
-    
+    return subGames[subgameID].movePlayed(turn, squareNum);
 }
 
-function getSubGameStatus()
+/**
+* public
+* This function queries the status of a subgame and returns whether it is over and who won or 
+* if it was a tie
+* @param {int}  subgameID   The ID of the subgame/canvas where the move was played
+* @return {int}             0 - still active, 1 - X won, 2 - Y won, 3 - game over in tie 
+*/
+function getSubGameStatus(subgameID)
 {
-    
+    return subGames[subgameID].winner;
 }
 
+
+function checkTicTacToes() {
+    var winner = 3;
+    if (subGames[0].winner != 0 && subGames[0].winner != 3 && subGames[0].winner ==             
+        subGames[1].winner && subGames[0].winner == subGames[2].winner) {
+		//alert("mainGame - detectGameWin() - win type 1");
+		winner = subGames[0].winner;
+		//winningSquares = [subGames[0].canvas, subGames[1].canvas, subGames[2].canvas];
+	}
+	else if (subGames[3].winner != 0 && subGames[3].winner != 3 && subGames[3].winner == 
+             subGames[4].winner && subGames[3].winner == subGames[5].winner) {
+		//alert("mainGame - detectGameWin() - win type 2");
+		winner = subGames[3].winner;
+		//winningSquares = [subGames[3].canvas, subGames[4].canvas, subGames[5].canvas];
+	}
+	else if (subGames[6].winner != 0 && subGames[6].winner != 3 && subGames[6].winner == 
+             subGames[7].winner && subGames[6].winner == subGames[8].winner) {
+		//alert("mainGame - detectGameWin() - win type 3");
+		winner = subGames[6].winner;
+		//winningSquares = [subGames[6].canvas, subGames[7].canvas, subGames[8].canvas];
+	}
+	else if (subGames[0].winner != 0 && subGames[0].winner != 3 && subGames[0].winner == 
+             subGames[3].winner && subGames[0].winner == subGames[6].winner) {
+		//alert("mainGame - detectGameWin() - win type 4");
+		winner = subGames[0].winner;
+		//winningSquares = [subGames[0].canvas, subGames[3].canvas, subGames[6].canvas];
+	}
+	else if (subGames[1].winner != 0 && subGames[1].winner != 3 && subGames[1].winner == 
+             subGames[4].winner && subGames[1].winner == subGames[7].winner) {
+		//alert("mainGame - detectGameWin() - win type 5");
+		winner = subGames[1].winner;
+		//winningSquares = [subGames[1].canvas, subGames[4].canvas, subGames[7].canvas];
+	}
+	else if (subGames[2].winner != 0 && subGames[2].winner != 3 && subGames[2].winner == 
+             subGames[5].winner && subGames[2].winner == subGames[8].winner) {
+		//alert("mainGame - detectGameWin() - win type 6");
+		winner = subGames[2].winner;
+		//winningSquares = [subGames[2].canvas, subGames[5].canvas, subGames[8].canvas];
+	}
+	else if (subGames[0].winner != 0 && subGames[0].winner != 3 && subGames[0].winner == 
+             subGames[4].winner && subGames[0].winner == subGames[8].winner) {
+		//alert("mainGame - detectGameWin() - win type 7");
+		winner = subGames[0].winner;
+		//winningSquares = [subGames[0].canvas, subGames[4].canvas, subGames[8].canvas];
+	}
+	else if (subGames[2].winner != 0 && subGames[2].winner != 3 && subGames[2].winner == 
+             subGames[4].winner && subGames[2].winner == subGames[6].winner) {
+		//alert("mainGame - detectGameWin() - win type 8");
+		winner = subGames[2].winner;
+		//winningSquares = [subGames[2].canvas, subGames[4].canvas, subGames[6].canvas];
+	}
+    return winner;
+}
+
+/**
+* public
+* This function returns the status of the entire game by saying if it is over and who won or
+* if the result is a tie
+* @return {int}             0 - still active, 1 - X won, 2 - Y won, 3 - game over in tie
+*/
 function getGameStatus()
 {
+    var winner = checkTicTacToes();
+
+    if (winner == 3) {
+        var xWins = 0;
+        var yWins = 0;
+        for s in subGames {
+            if (!s.gameOver) {
+                winner = 0;
+                break;
+            }
+            else {
+                if (s.winner == 1) {
+                    xWins++;
+                }
+                else if (s.winner == 2) {
+                    yWins++;
+                }
+            }
+        }
+        if (winner == 3) {
+            //all games are over and no one got a tic-tac-toe. 
+            //count the games won by each player
+            if (xWins > yWins) {
+                winner = 1;
+            }
+            else if (yWins > xWins) {
+                winner = 2;
+            }
+        } 
+    }
     
+    return winner;
 }
