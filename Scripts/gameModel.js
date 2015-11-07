040fc8c222207116d6b8207e329ecbcc3c4add9f
Nathaniel Hachten <Nathan@Nathaniels-MacBook-Air.local>
2015-11-07 16:17:24 -0500

    Added classes and stub functions to the model

---
+++ Scripts/gameModel.js
@@ -0,0 +1,27 @@
+// need global variable for the main game 
+var game = new MainGame();
+
+
+
+/**
+* This function processes moves to determine if the move is valid and if it is
+* updates its internal game state
+* @param {string}   canvasID    The ID of the subgame/canvas where the move was played
+* @param {string}   XorO        A string for whose move it is
+* @param {int}      squareNum   An int for the square within the subgame where the move was played
+* @return {boolean}             Returns whether the move was valid or not
+*/
+function movePlayed(canvasID, XorO, squareNum)
+{
+    
+}
+
+function getSubGameStatus()
+{
+    
+}
+
+function getGameStatus()
+{
+    
+}
