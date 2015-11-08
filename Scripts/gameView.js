46cb26f7e93bc33470fbc2501b7ae5db2c11279c
mchach24 <hach24mc@gmail.com>
2015-11-08 17:03:33 -0500

    Added alertGameOver function.

--- Scripts/gameView.js
+++ Scripts/gameView.js
@@ -160,3 +160,7 @@ function getSubGameId(subGameNum) {
     var idArray = ['#r1c1', '#r1c2','#r1c3','#r2c1','#r2c2','#r2c3','#r3c1','#r3c2','#r3c3'];
     return idArray[subGameNum];
 }
+
+function alertGameOver() {
+    alert('Won!');
+}
